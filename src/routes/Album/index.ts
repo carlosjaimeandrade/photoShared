84e0ec01488d570f0416691748e8865d2758ae35
Carlos Jaime <jaime_andrek@hotmail.com>
2023-02-01 19:49:59 -0300

    created validation for update of image and update of entity

--- src/routes/Album/index.ts
+++ src/routes/Album/index.ts
@@ -9,6 +9,8 @@ import AlbumController from '../../controllers/AlbumController';
 import albumMulterCreate from '../../helpers/album/albumMulterCreate'
 import albumMulterUpdate from '../../helpers/album/albumMulterUpdate'
 import Album from "../../Models/Album";
+import fs from 'fs';
+import { Op } from "sequelize";
 
 /**
  * Validator for checking if the image may to do uploading (create)
@@ -17,7 +19,6 @@ const uploadAlbumCreate = albumMulterCreate.import({
     storage: albumMulterCreate.storage,
     fileFilter: async (req, file, cb) => {
         const album = await Album.findOne({ where: { name: req.body.name }})
-        console.log(album)
         if(album == null){
             cb(null, true)
             return
@@ -25,12 +26,28 @@ const uploadAlbumCreate = albumMulterCreate.import({
         cb(null, false)
     }
 });
-const uploadAlbumUpdate = albumMulterUpdate.import({ storage: albumMulterUpdate.storage });
+
+/**
+ * Remove album photo and update
+ */
+const uploadAlbumUpdate = albumMulterUpdate.import({ 
+    storage: albumMulterUpdate.storage,
+    fileFilter: async (req, file, cb) => {
+        const album = await Album.findOne({ where: { id: req.params.id }})
+        const isDuplicate = await Album.findOne({ where: { name: req.body.name, id: { [Op.ne]: req.params.id } } })
+        if(album && !isDuplicate){
+            fs.unlinkSync('./' + album?.photographPath)
+            cb(null, true)
+            return
+        }
+        cb(null, false)
+    }
+});
 
 const albumRouter = express.Router();
 
 albumRouter.get('/album', AlbumController.get);
-albumRouter.post('/album',uploadAlbumCreate.single('photo'), AlbumController.create);
+albumRouter.post('/album', uploadAlbumCreate.single('photo'), AlbumController.create);
 albumRouter.put('/album/:id', uploadAlbumUpdate.single('photo'), AlbumController.update);
 
 export default albumRouter;
